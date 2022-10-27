d455aa5c9e9788ac2c465e04ecf861f36e6932ac
Ilona Shakurova <ishakuro@student.hive.fi>
2022-10-27 19:24:02 +0300

    Add theme

---
+++ client/src/theme.ts
@@ -0,0 +1,41 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// A custom theme for this app
+const theme = createTheme({
+	palette: {
+		background: {
+			default: "#fcfcfc"
+		},
+		primary: {
+			main: '#1976d2',
+		},
+		secondary: {
+			main: '#ffffff',
+		},
+		error: {
+			main: red.A400,
+		},
+	},
+});
+
+export const themeLight = createTheme({
+	palette: {
+		background: {
+			default: "#e4f0e2"
+		}
+	}
+});
+
+export const themeDark = createTheme({
+	palette: {
+		background: {
+			default: "#222222"
+		},
+		text: {
+			primary: "#ffffff"
+		}
+	}
+});
+
+export default theme;
