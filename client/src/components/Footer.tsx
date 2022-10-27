5197e35867871c71e061fcac2d6db3c9bde70689
Ilona Shakurova <ishakuro@student.hive.fi>
2022-10-27 19:25:53 +0300

    Add footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+const Footer = () => {
+	return (
+		<Typography sx={{ mt: 8, mb: 4 }} variant="body2" color="text.secondary" align="center">
+			{'Copyright © '}
+			<Link color="inherit" href="/">
+				Matcha
+			</Link>{'  by Ilona Shakurova & Alexei Shatalov  '}
+			{new Date().getFullYear()}.
+		</Typography>
+	);
+}
+
+export default Footer;
