1dd14ef2c6b95c1a769ace21076ca5a9989fa383
Ilona Shakurova <ishakuro@student.hive.fi>
2022-10-27 19:26:17 +0300

    Add client side app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import SignUpForm from './components/SignUpForm';
+import LoginForm from "./components/LoginForm";
+import Footer from "./components/Footer";
+
+import {
+	Routes,
+	Route,
+	Link,
+	// Navigate,
+} from 'react-router-dom'
+
+import {
+	Box,
+	Button,
+	AppBar,
+	Toolbar,
+	Typography,
+} from '@mui/material'
+
+import LoyaltyIcon from '@mui/icons-material/Loyalty';
+
+
+
+const App = () => {
+
+	return (
+		<Box>
+			<AppBar position="static" color="secondary" sx={{ justifyContent: "space-between" }} >
+				<Toolbar sx={{ justifyContent: "flex-end" }}>
+					<Typography
+						color="primary"
+						component={Link} to="/"
+						sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+					>
+						<LoyaltyIcon sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}/>
+					</Typography>
+					<Button color="inherit" component={Link} to="/login">Login</Button>
+					<Button color="inherit" component={Link} to="/signup">Sign Up</Button>
+					{/* {user
+							? <div>
+								<em>{user.name} logged in </em>
+								<Button onClick={handleLogout} color="inherit">Logout</Button>
+							</div>
+							: <Button color="inherit" component={Link} to="/login">
+								login
+							</Button>
+						} */}
+				</Toolbar>
+			</AppBar>
+			<Routes>
+				<Route path="/login" element={<LoginForm />} />
+				<Route path="/signup" element={<SignUpForm />} />
+			</Routes>
+			<Footer />
+		</Box>
+	);
+};
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import { CssBaseline } from '@mui/material';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<ThemeProvider theme={theme}>
+		{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+		<CssBaseline />
+		<Router>
+			<App />
+		</Router>
+	</ThemeProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
