69742fe951ba12f9eccbd5861401fa1ae68df964
Ilona Shakurova <leverseau19@gmail.com>
2022-10-28 12:07:01 +0300

    Add db config

--- server/index.ts
+++ server/index.ts
@@ -4,11 +4,27 @@ app.use(express.json());
 
 const PORT = 3000;
 
+import pool from './db';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const connectDb = async () => {
+	try {
+		const res = await pool.query('select 1 + 1');
+		console.log(res);
+		await pool.end();
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+void connectDb();
+
 app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+	console.log('someone pinged here');
+	res.send('pong');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
