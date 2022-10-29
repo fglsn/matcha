c5dde598c79bfb06e37c244e7ab975ed7b192a18
Ilona Shakurova <leverseau19@gmail.com>
2022-10-29 15:51:28 +0300

    Add new type parser helpers

--- server/src/dbUtil.ts
+++ server/src/dbUtil.ts
@@ -2,7 +2,21 @@ function getString(value: unknown): string {
 	if (typeof value === 'string') {
 		return value;
 	}
-	throw 'Expected value to be string, got: ' + value;
+	throw `Expected value to be string, got: ${typeof value}`;
 }
 
-export { getString };
+function getNumber(value: unknown): number {
+	if (typeof value === 'number') {
+		return value;
+	}
+	throw `Expected value to be number, got: ${typeof value}`;
+}
+
+function getBoolean(value: unknown): boolean {
+	if (typeof value === 'boolean') {
+		return value;
+	}
+	throw `Expected value to be boolean, got: ${typeof value}`;
+}
+
+export { getString, getNumber, getBoolean };
