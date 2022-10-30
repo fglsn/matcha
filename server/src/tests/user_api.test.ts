854c660da7345e11ca3fd2ce46bb3966ee186306
Ilona Shakurova <leverseau19@gmail.com>
2022-10-30 22:17:20 +0200

    Add test cases for user creation

---
+++ server/src/tests/user_api.test.ts
@@ -0,0 +1,97 @@
+import { describe, expect } from '@jest/globals';
+import supertest from 'supertest';
+
+import { app } from '../app';
+// import bcrypt from 'bcrypt';
+import { clearUsers, getAllUsers } from '../repositories/userRepository';
+import { newUser } from './test_helper';
+
+const api = supertest(app);
+
+jest.setTimeout(10000);
+
+const username = 'test';
+const email = 'test@test.com';
+const password = 'Test!111';
+const firstname = 'Test';
+const lastname = 'Testoff';
+
+describe('user creation', () => {
+	beforeEach(async () => {
+		await clearUsers();
+	});
+
+	test('creation succeeds with a new valid payload', async () => {
+		const usersAtStart = await getAllUsers();
+
+		await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(201)
+			.expect('Content-Type', /application\/json/);
+
+		const usersAtEnd = await getAllUsers();
+		expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+		const usernames = usersAtEnd.map((u) => u.username);
+		expect(usernames).toContain(newUser.username);
+	});
+
+	it.each([
+		[{ email, password, firstname, lastname }, 'Missing username'],
+		[{ username, password, firstname, lastname }, 'Missing email'],
+		[{ username, email, firstname, lastname }, 'Missing password'],
+		[{ username, email, password, lastname }, 'Missing first name'],
+		[{ username, email, password, firstname }, 'Missing last name'],
+
+		[{ username: 'tes', email, password, firstname, lastname }, 'Username is too short'],
+		[{ username: 'testtesttesttesttesttest', email, password, firstname, lastname }, 'Username is too long'], //24chars
+		[{ username: 'tes<3>', email, password, firstname, lastname }, 'Invalid username'],
+		[{ username: 'te st', email, password, firstname, lastname }, 'Invalid username'],
+		[{ username: 'te	st', email, password, firstname, lastname }, 'Invalid username'],
+		[{ username: 'te{st', email, password, firstname, lastname }, 'Invalid username'],
+
+		[{ username, email: '', password, firstname, lastname }, 'Invalid email'],
+		[{ username, email: 'testtest.com', password, firstname, lastname }, 'Invalid email'],
+		[{ username, email: '@test.com', password, firstname, lastname }, 'Invalid email'],
+		[{ username, email: '@test.com', password, firstname, lastname }, 'Invalid email'],
+		[{ username, email: 'test@@test.com', password, firstname, lastname }, 'Invalid email'],
+
+		[{ username, email, password: 'Test!1', firstname, lastname }, 'Password is too short'],
+		[{ username, email, password: 'Test!111Test!111Test!111Test!111Test!111Te', firstname, lastname }, 'Password is too long'], //42
+		[{ username, email, password: 'testtest', firstname, lastname }, 'Weak password'],
+		[{ username, email, password: '12345678', firstname, lastname }, 'Weak password'],
+		[{ username, email, password: '12345678', firstname, lastname }, 'Weak password'],
+		[{ username, email, password: 'T!111111', firstname, lastname }, 'Weak password'],
+		[{ username, email, password: 't!111111', firstname, lastname }, 'Weak password'],
+		[{ username, email, password: 'TestTest!', firstname, lastname }, 'Weak password'],
+		[{ username, email, password: 'Test11111', firstname, lastname }, 'Weak password'],
+
+		[{ username, email, password, firstname: 'testtesttesttesttesttest', lastname }, 'First name is too long'], //24
+		[{ username, email, password, firstname: ' ', lastname }, 'Invalid firstname'],
+		[{ username, email, password, firstname: '123', lastname }, 'Invalid firstname'],
+		[{ username, email, password, firstname: '<Test>', lastname }, 'Invalid firstname'],
+		[{ username, email, password, firstname: 'tes42', lastname }, 'Invalid firstname'],
+		[{ username, email, password, firstname: 'tes@t', lastname }, 'Invalid firstname'],
+
+		[{ username, email, password, firstname, lastname: 'testtesttesttesttesttesttesttesttesttesttesttest' }, 'Lastname is too long'],
+		[{ username, email, password, firstname, lastname: ' ' }, 'Invalid lastname'],
+		[{ username, email, password, firstname, lastname: '123' }, 'Invalid lastname'],
+		[{ username, email, password, firstname, lastname: '<Test>' }, 'Invalid lastname'],
+		[{ username, email, password, firstname, lastname: 'tes42' }, 'Invalid lastname'],
+		[{ username, email, password, firstname, lastname: 'tes@t' }, 'Invalid lastname']
+	])('creation fails with incorrect user payload', async (incorrectUser, expectedErrorMessage) => {
+		const usersAtStart = await getAllUsers();
+
+		const res = await api
+			.post('/api/users')
+			.send(incorrectUser)
+			.expect(400)
+			.expect('Content-Type', /application\/json/);
+
+		expect(res.body.error).toContain(expectedErrorMessage);
+
+		const usersAtEnd = await getAllUsers();
+		expect(usersAtEnd).toEqual(usersAtStart);
+	});
+});
