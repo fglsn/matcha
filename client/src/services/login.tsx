df26ae72130ccbdb760d96d31c8557596a345522
Ilona Shakurova <ishakuro@student.hive.fi>
2022-11-02 17:51:32 +0200

    Add error alert on login

--- client/src/services/login.tsx
+++ client/src/services/login.tsx
@@ -2,8 +2,12 @@ import axios from 'axios';
 import { apiBaseUrl } from '../constants';
 
 const login = async (credentials: { username: string, password: string }) => {
-	const response = await axios.post(`${apiBaseUrl}/login`, credentials);
-	return response.data;
+	try {
+		const response = await axios.post(`${apiBaseUrl}/login`, credentials);
+		return response.data;
+	} catch (err) {
+		return err.response.data;
+	}
 }
 
 const moduleExports = { login };
