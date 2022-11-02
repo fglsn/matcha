6cfeff2bfdbd6712fc12540e96750a14b5e51c16
Ilona Shakurova <ishakuro@student.hive.fi>
2022-11-02 15:51:28 +0200

    Set conditional navbar rendering based on if user is logged in or not

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,22 @@
+import { setLoggedUser, StateContext } from "../state";
+import { useContext } from "react";
+
 import { Link } from "react-router-dom";
 import { Button, AppBar, Toolbar, Typography } from '@mui/material';
 import LoyaltyIcon from '@mui/icons-material/Loyalty';
+import { AlertContext } from "./AlertProvider";
 
 const Navbar = () => {
+	const loggedUser = useContext(StateContext);
+	const alert = useContext(AlertContext);
+
+	const handleLogout = async (event: any) => {
+		event.preventDefault()
+		window.localStorage.clear()
+		dispatch(setLoggedUser(undefined))
+		alert.success('Logged out');
+	}
+
 	return (<AppBar
 		position="static"
 		color="secondary"
@@ -25,23 +39,25 @@ const Navbar = () => {
 					}}
 				/>
 			</Typography>
-			<Button color="inherit" component={Link} to="/login">
-				Login
-			</Button>
-			<Button color="inherit" component={Link} to="/signup">
-				Sign Up
-			</Button>
-			{/* {user
-				? <div>
-					<em>{user.name} logged in </em>
+			{loggedUser[0].loggedUser !== undefined
+				? <>
+					<em>{loggedUser[0].loggedUser?.username} logged in </em>
 					<Button onClick={handleLogout} color="inherit">Logout</Button>
-				</div>
-				: <Button color="inherit" component={Link} to="/login">
-					login
-				</Button>
-			} */}
+				</>
+				: <>
+					<Button color="inherit" component={Link} to="/login">
+						Login
+					</Button>
+					<Button color="inherit" component={Link} to="/signup">
+						Sign Up
+					</Button></>
+			}
 		</Toolbar>
 	</AppBar>);
 };
 
 export default Navbar;
+
+function dispatch(arg0: { type: "SET_LOGGED_USER"; payload: import("../types").LoggedUser; }) {
+	throw new Error("Function not implemented.");
+}
