fc31954badfce60b9bea1e8c9f75c7f1344435f6
Ilona Shakurova <ishakuro@student.hive.fi>
2022-11-02 20:48:06 +0200

    Add mailer

---
+++ server/src/utils/mailer.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+
+export const Mailer = (to: string, subject: string, text: string) => {
+
+	const transporter = nodemailer.createTransport({
+		service: 'hotmail',
+		auth: {
+			user: 'matcha_web@hotmail.com',
+			pass: 'Test!111'
+		}
+	});
+
+	transporter.sendMail({ from: 'matcha_web@hotmail.com', to, subject, html: text }, (err, info) => {
+		if (err) {
+			console.error("Error: ", err);
+			return err;
+		} else {
+			console.log(info);
+			return true;
+		}
+	});
+};
