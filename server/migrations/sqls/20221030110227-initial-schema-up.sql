2b88333ad12d1577b0d8e3342d63401bd035f714
Ilona Shakurova <leverseau19@gmail.com>
2022-11-07 14:18:40 +0200

    Create new table Sessions

--- server/migrations/sqls/20221030110227-initial-schema-up.sql
+++ server/migrations/sqls/20221030110227-initial-schema-up.sql
@@ -1,6 +1,6 @@
 create table users
 (
-	id bigserial primary key,
+	id bigserial primary key unique,
 	username varchar not null unique,
 	email varchar not null unique,
 	password_hash varchar not null,
@@ -10,3 +10,14 @@ create table users
 	created_at timestamptz not null default now(),
 	activation_code varchar not null unique
 );
+
+create table user_sessions
+(
+	session_id uuid default gen_random_uuid() primary key,
+	user_id bigserial not null,
+	username varchar not null,
+	email varchar not null,
+	expires_at timestamptz not null default now() + time '24:00'
+);
+
+create index user_sessions_user_id on user_sessions (user_id);
