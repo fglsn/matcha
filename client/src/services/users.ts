60e708950cbce5196be9c0fae0f3f7d7e14c44c6
Ilona Shakurova <leverseau19@gmail.com>
2022-11-10 16:54:04 +0200

    Comments

--- client/src/services/users.ts
+++ client/src/services/users.ts
@@ -2,27 +2,28 @@ import axios from 'axios';
 import { apiBaseUrl } from '../constants';
 import { NewUser } from '../types';
 
+// Create new user or throw
 const create = async (newObject: NewUser): Promise<any> => {
-	try {
-		const response = await axios.post(`${apiBaseUrl}/users`, newObject);
-		return response.data;
-	} catch (err) {
-		return err.response.data;
-	}
+	const response = await axios.post(`${apiBaseUrl}/users`, newObject);
+	return response.data;
 };
 
+// Activate new user or throw
 const activate = async (activationCode: string): Promise<void> => {
 	await axios.get(`${apiBaseUrl}/users/activate/${activationCode}`);
 };
 
+// Validate request and send reset link by email or throw
 const requestPasswordReset = async (email: string): Promise<void> => {
 	await axios.post(`${apiBaseUrl}/users/forgot_password`, { email: email });
 };
 
+// Validate reset link on get request or throw
 const checkResetToken = async (resetToken: string): Promise<void> => {
 	await axios.get(`${apiBaseUrl}/users/forgot_password/${resetToken}`);
 };
 
+// Update users password or throw
 const resetPassword = async (
 	token: string,
 	passwordPlain: string
@@ -32,13 +33,10 @@ const resetPassword = async (
 	});
 };
 
+// Get data for profile or throw
 const getUserData = async (userId: string): Promise<any> => {
-	try {
-		const response = await axios.get(`${apiBaseUrl}/users/${userId}`);
-		return response.data;
-	} catch (err) {
-		return err.response.data;
-	}
+	const response = await axios.get(`${apiBaseUrl}/users/${userId}`);
+	return response.data;
 };
 
 const moduleExports = {
