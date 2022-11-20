87d1cb7724b44e68b987750629b2810f10b20e3b
Ilona Shakurova <leverseau19@gmail.com>
2022-11-20 17:27:18 +0200

    Small refactor

--- server/src/routes/account.ts
+++ server/src/routes/account.ts
@@ -12,15 +12,12 @@ router.get(
 	'/:id',
 	sessionExtractor,
 	asyncHandler(async (req: CustomRequest, res) => {
-		if (req.session) {
-			if (req.session.userId === req.params.id && req.session.userId) {
-				const result = await getUserDataByUserId(req.session.userId);
-				res.status(200).json(result);
-				return;
-			}
+		if (!req.session || !req.session.userId || req.session.userId !== req.params.id) {
 			throw new AppError(`No rights to get profile data`, 400);
-			// res.status(400).json({ error: `Bad Request: no rights to get profile data with id: ${req.params.id}` });
 		}
+		const result = await getUserDataByUserId(req.session.userId);
+		res.status(200).json(result);
+		// return;
 	})
 );
 
@@ -28,18 +25,15 @@ router.put(
 	'/:id',
 	sessionExtractor,
 	asyncHandler(async (req: CustomRequest, res) => {
-		if (req.session) {
-			if (req.session.userId === req.params.id && req.session.userId) {
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-				const updatedProfile = parseUserProfilePayload(req.body);
-				await updateUserDataByUserId(req.session.userId, updatedProfile);
-				//res.status(200).json(updatedProfile);
-				res.status(200).end();
-				return;
-			}
+		if (!req.session || !req.session.userId || req.session.userId !== req.params.id) {
 			throw new AppError(`No rights to update profile data`, 400);
-			// res.status(400).json({ error: `No rights to update profile data with id: ${req.params.id}` });
 		}
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+		const updatedProfile = parseUserProfilePayload(req.body);
+		await updateUserDataByUserId(req.session.userId, updatedProfile);
+		//res.status(200).json(updatedProfile);
+		res.status(200).end();
+		// return;
 	})
 );
 
