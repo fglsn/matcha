c88c0ff2802cd70b3e8664fdd347a299f85ee552
Ilona Shakurova <leverseau19@gmail.com>
2022-11-24 16:56:48 +0200

    Add auth error handling (logout, alert) to useServiceCall

--- client/src/hooks/useServiceCall.tsx
+++ client/src/hooks/useServiceCall.tsx
@@ -1,10 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
+import { AlertContext } from '../components/AlertProvider';
+import { logoutUser } from '../services/logout';
+import { useStateValue } from '../state/state';
+import { AuthError } from '../utils/errors';
 
-export const useServiceCall = (serviceFn: () => Promise<any | undefined>, dependencies: any[]) => {
+export const useServiceCall = (
+	serviceFn: () => Promise<any | undefined>,
+	dependencies: any[]
+) => {
 	const [data, setData] = useState<any>();
 	const [error, setError] = useState<any>();
 	const [loading, setLoading] = useState<boolean>();
+	const [, dispatch] = useStateValue();
+	const { error: errorCallback } = useContext(AlertContext);
 
 	const callback = useCallback(() => {
 		const fetchData = async () => {
@@ -14,7 +23,9 @@ export const useServiceCall = (serviceFn: () => Promise<any | undefined>, depend
 				setData(res);
 				setLoading(false);
 			} catch (err) {
+				if (err instanceof AuthError) logoutUser(dispatch);
 				setError(err);
+				errorCallback(err.message);
 				setLoading(false);
 			}
 		};
