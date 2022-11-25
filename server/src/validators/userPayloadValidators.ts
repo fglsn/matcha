7c2eb7dae03f8c11f2e8ce9f7babfae1376cb0f0
Ilona Shakurova <leverseau19@gmail.com>
2022-11-25 18:23:39 +0200

    Typo

--- server/src/validators/userPayloadValidators.ts
+++ server/src/validators/userPayloadValidators.ts
@@ -244,7 +244,7 @@ export const parseLocationString = (location: unknown): string => {
 		throw new ValidationError(`Missing location string.`);
 	}
 	if (!isString(location)) {
-		throw new ValidationError(`Expected bio to be string, got: ${typeof location}`);
+		throw new ValidationError(`Expected lovation to be string, got: ${typeof location}`);
 	}
 	return location;
 };
