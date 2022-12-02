1b2bcbfb06b0059f36f081fb4bfcf9e9521ed4b2
Ilona Shakurova <leverseau19@gmail.com>
2022-12-02 23:06:29 +0200

    Move reapiting test functions into helper

---
+++ server/src/tests/test_helper_fns.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import supertest from 'supertest';
+import { app } from '../app';
+import { infoProfile, defaultCoordinates, ipAddress } from './test_helper';
+import { DataURL } from './test_helper_images';
+import { findUserByUsername } from '../repositories/userRepository';
+import { requestCoordinatesByIp, getLocation } from '../services/location';
+import { createNewUser } from '../services/users';
+import { NewUser } from '../types';
+// import { clearSessions } from '../repositories/sessionRepository';
+
+export const api = supertest(app);
+
+export let id = <string>'';
+export let loginRes = <supertest.Response>{};
+
+jest.mock('../services/location');
+export const requestCoordinatesByIpMock = jest.mocked(requestCoordinatesByIp);
+export const getLocationMock = jest.mocked(getLocation);
+
+export const initLoggedUser = async (username: string, loginUser: { username: string; password: string }) => {
+	const user = await findUserByUsername(username);
+	const activationCode = user?.activationCode;
+	await api.post(`/api/users/activate/${activationCode}`);
+	const res = await api.post('/api/login').send(loginUser).expect(200);
+	return res;
+};
+
+export const putToProfile = async (id: string) => {
+	getLocationMock.mockReturnValue(Promise.resolve('Helsinki, Finland'));
+	await api
+		.put(`/api/users/${id}/profile`)
+		.set({ Authorization: `bearer ${loginRes.body.token}` })
+		.send(infoProfile)
+		.expect(200);
+	// if (res.body.error)
+	// 	console.log(res.body.error);
+};
+
+export const postToPhotos = async (id: string) => {
+	await api
+		.post(`/api/users/${id}/photos`)
+		.set({ Authorization: `bearer ${loginRes.body.token}` })
+		.send({ images: [{ dataURL: DataURL }] })
+		.expect(200);
+};
+
+export const loginAndPrepareUser = async (user: NewUser, loginUser: { username: string; password: string }) => {
+	requestCoordinatesByIpMock.mockReturnValue(Promise.resolve(defaultCoordinates));
+	await createNewUser(user, ipAddress);
+	loginRes = await initLoggedUser(user.username, loginUser);
+	id = <string>JSON.parse(loginRes.text).id;
+	await Promise.all([putToProfile(id), postToPhotos(id)]);
+	return { id: id, token: loginRes.body.token };
+};
