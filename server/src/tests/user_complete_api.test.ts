e71ea992828bd1bb8f753614c5f1749c74de6720
Ilona Shakurova <leverseau19@gmail.com>
2022-12-03 12:57:21 +0200

    Reverse latest unnesessary refactor in user_complete_test file

--- server/src/tests/user_complete_api.test.ts
+++ server/src/tests/user_complete_api.test.ts
@@ -2,11 +2,12 @@ import pool from '../db';
 import supertest from 'supertest';
 import { app } from '../app';
 import { describe, expect } from '@jest/globals';
-import { newUser, loginUser, defaultCoordinates, ipAddress, completenessFalse, completenessTrue } from './test_helper';
-import { initLoggedUser, putToProfile, postToPhotos } from './test_helper_fns';
-import { clearUsers } from '../repositories/userRepository';
+import { newUser, loginUser, defaultCoordinates, ipAddress, completenessFalse, completenessTrue, infoProfile } from './test_helper';
+import { getLocationMock } from './test_helper_fns';
+import { clearUsers, findUserByUsername } from '../repositories/userRepository';
 import { requestCoordinatesByIp } from '../services/location';
 import { createNewUser } from '../services/users';
+import { DataURL } from './test_helper_images';
 
 const api = supertest(app);
 jest.setTimeout(10000);
@@ -16,12 +17,39 @@ const requestCoordinatesByIpMock = jest.mocked(requestCoordinatesByIp);
 let loginRes = <supertest.Response>{};
 let id = <string>'';
 
+export const initLoggedUser = async () => {
+	const user = await findUserByUsername(newUser.username);
+	const activationCode = user?.activationCode;
+	await api.post(`/api/users/activate/${activationCode}`);
+	const res = await api.post('/api/login').send(loginUser).expect(200);
+	return res;
+};
+
+export const putToProfile = async () => {
+	getLocationMock.mockReturnValue(Promise.resolve('Helsinki, Finland'));
+	await api
+		.put(`/api/users/${id}/profile`)
+		.set({ Authorization: `bearer ${loginRes.body.token}` })
+		.send(infoProfile)
+		.expect(200);
+	// if (res.body.error)
+	// 	console.log(res.body.error);
+};
+
+export const postToPhotos = async () => {
+	await api
+		.post(`/api/users/${id}/photos`)
+		.set({ Authorization: `bearer ${loginRes.body.token}` })
+		.send({ images: [{ dataURL: DataURL }] })
+		.expect(200);
+};
+
 describe('Check repsonse of GET to /api/users/:id/complete', () => {
 	beforeEach(async () => {
 		await clearUsers();
 		requestCoordinatesByIpMock.mockReturnValue(Promise.resolve(defaultCoordinates));
 		await createNewUser(newUser, ipAddress);
-		loginRes = await initLoggedUser(newUser.username, loginUser);
+		loginRes = await initLoggedUser();
 		id = <string>JSON.parse(loginRes.text).id;
 		// resFromProfile = await getResFromProfile(loginRes);
 	});
@@ -32,22 +60,22 @@ describe('Check repsonse of GET to /api/users/:id/complete', () => {
 		expect(JSON.parse(resFromComplete.text)).toEqual(completenessFalse);
 	});
 	test('should respond 200 and competness: false (no photos)', async () => {
-		await putToProfile(id);
+		await putToProfile();
 		const resFromComplete = await api.get(`/api/users/${id}/complete`).set({ Authorization: `bearer ${loginRes.body.token}` });
 
 		expect(resFromComplete.statusCode).toBe(200);
 		expect(JSON.parse(resFromComplete.text)).toEqual(completenessFalse);
 	});
 	test('should respond 200 and competness: false (no basic user data)', async () => {
-		await postToPhotos(id);
+		await postToPhotos();
 		const resFromComplete = await api.get(`/api/users/${id}/complete`).set({ Authorization: `bearer ${loginRes.body.token}` });
 
 		expect(resFromComplete.statusCode).toBe(200);
 		expect(JSON.parse(resFromComplete.text)).toEqual(completenessFalse);
 	});
 	test('should respond 200 and competness: true (all completed)', async () => {
-		await putToProfile(id);
-		await postToPhotos(id);
+		await putToProfile();
+		await postToPhotos();
 		const resFromComplete = await api.get(`/api/users/${id}/complete`).set({ Authorization: `bearer ${loginRes.body.token}` });
 		expect(resFromComplete.statusCode).toBe(200);
 		expect(JSON.parse(resFromComplete.text)).toEqual(completenessTrue);
@@ -55,7 +83,7 @@ describe('Check repsonse of GET to /api/users/:id/complete', () => {
 
 	describe('returns false if any on the required fields were left empty', () => {
 		test('false on null birthday', async () => {
-			await postToPhotos(id);
+			await postToPhotos();
 			const query = {
 				text: 'update users set birthday = $2, gender = $3, orientation = $4, bio = $5, tags = $6 where id = $1',
 				values: [id, null, 'male', 'gay', 'hehehehehehheheheheeheh', ['Sauna']]
@@ -67,7 +95,7 @@ describe('Check repsonse of GET to /api/users/:id/complete', () => {
 		});
 
 		test('false on null gender', async () => {
-			await postToPhotos(id);
+			await postToPhotos();
 			const query = {
 				text: 'update users set birthday = $2, gender = $3, orientation = $4, bio = $5, tags = $6 where id = $1',
 				values: [id, '1988-01-30', null, 'gay', 'hehehehehehheheheheeheh', ['Sauna']]
@@ -79,7 +107,7 @@ describe('Check repsonse of GET to /api/users/:id/complete', () => {
 		});
 
 		test('false on null orientation', async () => {
-			await postToPhotos(id);
+			await postToPhotos();
 			const query = {
 				text: 'update users set birthday = $2, gender = $3, orientation = $4, bio = $5, tags = $6 where id = $1',
 				values: [id, '1988-01-30', 'male', null, 'hehehehehehheheheheeheh', ['Sauna']]
@@ -91,7 +119,7 @@ describe('Check repsonse of GET to /api/users/:id/complete', () => {
 		});
 
 		test('false on null bio', async () => {
-			await postToPhotos(id);
+			await postToPhotos();
 			const query = {
 				text: 'update users set birthday = $2, gender = $3, orientation = $4, bio = $5, tags = $6 where id = $1',
 				values: [id, '1988-01-30', 'male', 'gay', null, ['Sauna']]
@@ -103,7 +131,7 @@ describe('Check repsonse of GET to /api/users/:id/complete', () => {
 		});
 
 		test('false on null tags', async () => {
-			await postToPhotos(id);
+			await postToPhotos();
 			const query = {
 				text: 'update users set birthday = $2, gender = $3, orientation = $4, bio = $5, tags = $6 where id = $1',
 				values: [id, '1988-01-30', 'male', 'gay', 'hehehehehehheheheheeheh', null]
