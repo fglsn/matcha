65dc3b922fe65efdb934c38d0c3cde1ccb94a8a3
Ilona Shakurova <leverseau19@gmail.com>
2022-12-08 20:45:31 +0200

    Change populators

--- server/src/populateLikes.ts
+++ server/src/populateLikes.ts
@@ -1,17 +1,13 @@
 import dotenv from 'dotenv';
-import { addLikeEntry, checkLikeEntry } from './repositories/likesRepository';
-import { addMatchEntry } from './repositories/matchesRepository';
 import { getIdList } from './repositories/userRepository';
+import { likeUser } from './services/users';
 
 const populateLikesAndMatches = async () => {
 	const idList = await getIdList();
 	for (let i = 0; i < idList.length; i++) {
 		const j = (idList.length * Math.random()) | 0;
 		if (idList[i].id !== idList[j].id) {
-			await addLikeEntry(idList[i].id, idList[j].id);
-			if (await checkLikeEntry(idList[j].id, idList[i].id)) {
-				await addMatchEntry(idList[i].id, idList[j].id);
-			}
+			await likeUser(idList[i].id, idList[j].id);
 		}
 	}
 };
