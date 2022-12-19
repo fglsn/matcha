a1c0ed970d97791152754f17a69442fa68298d1e
Aleksei Shatalov <aalleexsh2011@yandex.ru>
2022-12-19 21:04:32 +0200

    Chat: Client. ReloadContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,6 +27,7 @@ import Matches from './components/Matches';
 import Blocks from './components/Blocks';
 import ResponsiveDrawer from './components/Drawer';
 import Chats from './components/Chats';
+import { ChatReloadProvider } from './components/ChatReloadProvider';
 
 const MinWidthContainer = styled.div`
 	min-width: fit-content;
@@ -67,30 +68,33 @@ const App = () => {
 			<MinWidthContainer style={{ display: 'flex' }}>
 				<SnackbarProvider>
 					<AlertProvider>
-						<ResponsiveDrawer />
-						<Box style={{ textAlign: 'center', flexGrow: 1, position: 'relative', top: '5rem' }}>
-							<AlertSnackBar />
-							<Routes>
-								<Route path="/" element={<Main />} />
-								{/* prettier-ignore */}
-								<Route path="/login" element={!loggedUser ? <LoginForm /> : <Navigate to="/"/>} />
-								{/* prettier-ignore */}
-								<Route path="/signup" element={!loggedUser ? <SignUpForm /> : <Navigate to="/"/>} />
-								{/* prettier-ignore */}
-								<Route path="/forgot_password" element={!loggedUser ? <ForgotPassword /> : <Navigate to="/"/>} />
-								<Route path="/profile" element={<ProfileEditor />} />
-								<Route path="/profile/:id" element={<PublicProfile />} />
-								<Route path="/update_email" element={<UpdateEmail />} />
-								<Route path="/visit_history" element={<VisitHistory />} />
-								<Route path="/likes" element={<Likes />} />
-								<Route path="/matches" element={<Matches />} />
-								<Route path="/blocks" element={<Blocks />} />
-								<Route path="/chats" element={<Chats />} />
-								<Route path="/testAuth" element={<TestAuth />} />
-								<Route path="*" element={<Navigate to="/" replace />} />
-							</Routes>
-							<Footer />
-						</Box>
+						<ChatReloadProvider>
+							<ResponsiveDrawer />
+							<Box style={{ textAlign: 'center', flexGrow: 1, position: 'relative', top: '5rem' }}>
+								<AlertSnackBar />
+								<Routes>
+									<Route path="/" element={<Main />} />
+									{/* prettier-ignore */}
+									<Route path="/login" element={!loggedUser ? <LoginForm /> : <Navigate to="/"/>} />
+									{/* prettier-ignore */}
+									<Route path="/signup" element={!loggedUser ? <SignUpForm /> : <Navigate to="/"/>} />
+									{/* prettier-ignore */}
+									<Route path="/forgot_password" element={!loggedUser ? <ForgotPassword /> : <Navigate to="/"/>} />
+									<Route path="/profile" element={<ProfileEditor />} />
+									<Route path="/profile/:id" element={<PublicProfile />} />
+									<Route path="/update_email" element={<UpdateEmail />} />
+									<Route path="/visit_history" element={<VisitHistory />} />
+									<Route path="/likes" element={<Likes />} />
+									<Route path="/matches" element={<Matches />} />
+									<Route path="/blocks" element={<Blocks />} />
+									<Route path="/chats" element={<Chats />} />
+									<Route path="/chats/:id" element={<ChatWindow />} />
+									<Route path="/testAuth" element={<TestAuth />} />
+									<Route path="*" element={<Navigate to="/" replace />} />
+								</Routes>
+								<Footer />
+							</Box>
+						</ChatReloadProvider>
 					</AlertProvider>
 				</SnackbarProvider>
 			</MinWidthContainer>
