5f707c424fefce037356c3009b7afbfc88c87ad0
Ilona Shakurova <leverseau19@gmail.com>
2022-12-26 20:47:06 +0200

    Add sort & filter view to main search page

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,6 +1,9 @@
-import { Alert, Container } from '@mui/material';
+//prettier-ignore
+import { Alert, Box, ToggleButtonGroup, ToggleButton, Button, Container, Popper, styled, Paper, Grid, Slider, Typography } from '@mui/material';
 import { useState } from 'react';
 import { useServiceCall } from '../hooks/useServiceCall';
+import { useToggleButton } from '../hooks/useToggleButton';
+import { useRangeSlider } from '../hooks/useRangeSlider';
 import { getMatchSuggestions } from '../services/search';
 import { useStateValue } from '../state';
 import { ProfilePublic } from '../types';
@@ -8,8 +11,64 @@ import LoadingIcon from './LoadingIcon';
 import withProfileRequired from './ProfileRequired';
 import PublicProfile from './PublicProfile/PublicProfile';
 
+export const StyledMain= styled('div')`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const StyledPaper = styled(Paper)`
+	display: flex;
+	padding: 1.5rem;
+	background-color: ##ffc600db;
+	overflow-y: scroll;
+	z-index: 2;
+	position: initial;
+`;
+
+const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
+	'& .MuiToggleButtonGroup-grouped': {
+		margin: theme.spacing(0.5),
+		border: 3,
+		'&.Mui-disabled': {
+			border: 3
+		},
+		'&:not(:first-of-type)': {
+			borderRadius: theme.shape.borderRadius
+		},
+		'&:first-of-type': {
+			borderRadius: theme.shape.borderRadius
+		}
+	}
+}));
+
+const getMaxDistanceLabel = (value: number) => {
+	if (value >= 200) return `${value}+ km`;
+	return `${value} km`;
+};
+
+const getMaxAgeLabel = (value: number) => {
+	if (value >= 65) return `${value}+ yo`;
+	return `${value} yo`;
+};
+
 const Main = () => {
 	const [{ loggedUser }] = useStateValue();
+
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+	const togglePopper = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(anchorEl ? null : event.currentTarget);
+	};
+
+	const open = Boolean(anchorEl);
+	const id = open ? 'popper' : undefined;
+
+	const sortBy = useToggleButton('distance');
+
+	const distanceRangeSlider = useRangeSlider([2, 50], 2, 200);
+	const ageRangeSlider = useRangeSlider([18, 65], 18, 65);
+	const ratingRangeSlider = useRangeSlider([45, 100], 1, 100);
+	const tagsRangeSlider = useRangeSlider([1, 5], 1, 5);
 
 	const {
 		data: matchSuggestionsData,
@@ -26,7 +85,90 @@ const Main = () => {
 		return <LoadingIcon />;
 	}
 
-	return <Profiles matchSuggestionsData={matchSuggestionsData} />;
+	return (
+		<StyledMain>
+			<Button onClick={togglePopper} aria-describedby={id}>
+				Sort & Filter
+			</Button>
+			<Popper
+				id={id}
+				open={open}
+				anchorEl={anchorEl}
+				placement="bottom-start"
+				disablePortal={true}
+				style={{ zIndex: '5' }}
+				modifiers={[
+					{
+						name: 'preventOverflow',
+						enabled: true,
+						options: {
+							altAxis: true,
+							altBoundary: true,
+							tether: true,
+							rootBoundary: 'document',
+							padding: 8
+						}
+					}
+				]}
+			>
+				<StyledPaper>
+					<Grid item sm={'auto'} mt={1}>
+						<strong>Sort by</strong>
+						<Box sx={{ flexDirection: 'column' }}>
+							<StyledToggleButtonGroup exclusive {...sortBy}>
+								<ToggleButton value="distance">DISTANCE</ToggleButton>
+								<ToggleButton value="age">AGE</ToggleButton>
+								<ToggleButton value="rating">RATING</ToggleButton>
+								<ToggleButton value="tags">COMMON INTERESTS</ToggleButton>
+							</StyledToggleButtonGroup>
+						</Box>
+						<Box sx={{ flexDirection: 'column' }}>
+							<strong>Filter by</strong>
+							<div>
+								<Typography gutterBottom>Distance:</Typography>
+								<Slider
+									{...distanceRangeSlider}
+									valueLabelDisplay="auto"
+									disableSwap
+									getAriaValueText={getMaxDistanceLabel}
+									valueLabelFormat={getMaxDistanceLabel}
+								/>
+							</div>
+							<div>
+								<Typography gutterBottom>Age:</Typography>
+								<Slider
+									{...ageRangeSlider}
+									disableSwap
+									valueLabelDisplay="auto"
+									getAriaValueText={getMaxAgeLabel}
+									valueLabelFormat={getMaxAgeLabel}
+								/>
+							</div>
+							<div>
+								<Typography gutterBottom>Rating:</Typography>
+								<Slider
+									{...ratingRangeSlider}
+									valueLabelDisplay="auto"
+									disableSwap
+								/>
+							</div>
+							<div>
+								<Typography gutterBottom>
+									Number of common interests:
+								</Typography>
+								<Slider
+									{...tagsRangeSlider}
+									valueLabelDisplay="auto"
+									disableSwap
+								/>
+							</div>
+						</Box>
+					</Grid>
+				</StyledPaper>
+			</Popper>
+			<Profiles matchSuggestionsData={matchSuggestionsData} />
+		</StyledMain>
+	);
 };
 
 const Profiles = ({
