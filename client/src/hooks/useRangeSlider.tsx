c8fb9656f86b9d0ab25bd0cad45b50cd70342181
Ilona Shakurova <ishakuro@student.hive.fi>
2023-01-09 20:34:46 +0200

    Fix SliderHook (Maximum update depth exceeded)

--- client/src/hooks/useRangeSlider.tsx
+++ client/src/hooks/useRangeSlider.tsx
@@ -2,24 +2,13 @@ import { useState } from 'react';
 
 export const useRangeSlider = (initialValue: number[], min: number, max: number) => {
 	const [value, setValue] = useState(initialValue);
-	const minRange = 0;
-
+	
 	const onChange = (event: Event, newValue: number | number[], activeThumb: number) => {
 		if (!Array.isArray(newValue)) {
 			return;
 		}
-
-		if (newValue[1] - newValue[0] < minRange) {
-			if (activeThumb === 0) {
-				const clamped = Math.min(newValue[0], max - minRange);
-				setValue([clamped, clamped + minRange]);
-			} else {
-				const clamped = Math.max(newValue[1], minRange);
-				setValue([clamped - minRange, clamped]);
-			}
-		} else {
+		if (value[0] !== newValue[0] || value[1] !== newValue[1])
 			setValue(newValue as number[]);
-		}
 	};
 
 	return {
